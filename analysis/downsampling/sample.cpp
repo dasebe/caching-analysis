b96b45df815ac0b39dfc65c9a9a4672c1a997545
Daniel S. Berger <berger@cs.uni-kl.de>
2019-06-20 10:31:16 -0400

    first sampler prototype

--- analysis/downsampling/sample.cpp
+++ analysis/downsampling/sample.cpp
@@ -2,6 +2,7 @@
 #include <iostream>
 #include <string>
 #include <random>
+#include <unordered_map>
  
 using namespace std;
 
@@ -22,17 +23,43 @@ int main (int argc, char* argv[])
     ifstream infile(inputFile);
     ofstream outfile(outputFile);
 
-    std::random_device rd;
-    std::mt19937 gen(rd());
-    std::bernoulli_distribution d(sampleFactor);
+    cerr<< "started "
+        << inputFile << " "
+        << outputFile << " "
+        << sampleFactor << "\n";
+    
+    random_device rd;
+    mt19937 gen(rd());
+    bernoulli_distribution sampledecision(sampleFactor);
 
+    uint64_t sampled_objs = 0, sampled_reqs = 0, total_reqs = 0;
+    unordered_map<int64_t, bool> samplethisobject;
     int64_t t, id, size, tmp;
     while (infile >> t >> id >> size >> tmp)
     {
-        cout << d(gen) << "\n";
+        total_reqs++;
+        if(samplethisobject.count(id)==0) {
+            // new object to be decided
+            const bool curdecision= sampledecision(gen);
+            samplethisobject[id] = curdecision;
+            sampled_objs += curdecision;
+        } else {
+            if(samplethisobject[id]) {
+                // known object, which should be sampled
+                sampled_reqs++;
+                outfile << t << " " << id << " " << size << " " << tmp << "\n";
+            }
+        }
     }
 
     infile.close();
+    outfile.close();
+
+    cout
+        << inputFile << " "
+        << sampleFactor << " "
+        << sampled_objs/double(samplethisobject.size()) << " "
+        << sampled_reqs/double(total_reqs) << "\n";
 
     return 0;
 }
