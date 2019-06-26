aa82ae734af9b19b89db002552632988fab33cb1
Daniel S Berger <dsberger@cmu.edu>
2019-06-26 10:23:48 -0400

    old changed to trace generator

--- synth/tracegen.cpp
+++ synth/tracegen.cpp
@@ -16,53 +16,68 @@ typedef list<fi_pair_t>::iterator list_iterator_t;
 
 int main (int argc, char* argv[])
 {
-  // parameters
-  if(argc!=4) {
-    cout << "\n inputfile outputfile repetitions\n";
-    return 1;
-  }
-  const string inputfile(argv[1]);
-  const string outputfile(argv[2]);
-  const long reps = atol(argv[3]);
-
-  ifstream infile;
-  infile.open(inputfile);
-  random_device rd;
-  mt19937 rnd_gen (rd ());
-  int64_t tsize;
-  long double trate;
-  list<fi_pair_t> reqseq;
-  vector<long> size;
-  long i=0;
-  while(infile >> trate >> tsize) {
-    size.push_back(tsize);
-    exponential_distribution<> iaRandH (trate);
-    long double globalTime = iaRandH(rnd_gen);
-    while(globalTime<reps) {
-      reqseq.push_back(fi_pair_t(globalTime,i,0));
-      globalTime += iaRandH(rnd_gen);
+    // parameters
+    if(argc!=4) {
+        cout << "\n inputfile outputfile maxtime\n";
+        return 1;
     }
-    i++; //that's the "id"
-  }
-  infile.close();
+    const string inputfile(argv[1]);
+    const string outputfile(argv[2]);
+    const long double maxTime = std::stold(argv[3]);
 
-  // sort tuples lexicographically, i.e., by time
-  cout << "finished raw req sequence.\n";
-  reqseq.sort();
-  cout << "finished sorting req.\n";
+    ifstream infile;
+    infile.open(inputfile);
+    random_device rd;
+    mt19937 rnd_gen (rd ());
+    int64_t tsize;
+    long double trate = 1e12;
+    list<fi_pair_t> reqseq;
+    vector<long> size;
+    long i=0;
 
-  ofstream outfile;
-  outfile.open(outputfile);
-  outfile<<fixed<<setprecision(0); // turn of scientific notation
+    std::uniform_real_distribution<double> uniform(0.0,maxTime);
 
-  // output request sequence
-  list_iterator_t rit;
-  for (rit=reqseq.begin(); rit != reqseq.end(); ++rit) {
-    outfile << 1000*get<0>(*rit) << " " << get<1>(*rit) << " " << size[get<1>(*rit)] << "\n"; 
-  }
+    while(infile >> tsize) {
+        if(tsize<1 || tsize > 4096) {
+            continue;
+        }
+        cerr << trate << "\n";
+        size.push_back(tsize);
+        exponential_distribution<> iaRandH (trate);
+        long double globalTime = iaRandH(rnd_gen);
+        if(globalTime>maxTime) {
+            // treat as one-hit-wonder
+            globalTime = uniform(rnd_gen);
+            //            cerr << globalTime << " " << i << "\n";
+            reqseq.push_back(fi_pair_t(globalTime,i,0));
+        } else {
+            while(globalTime<maxTime) {
+                reqseq.push_back(fi_pair_t(globalTime,i,0));
+                globalTime += iaRandH(rnd_gen);
+            }
+        }
+        i++; //that's the "id"
+        trate /= 1.5;
+    }
+    infile.close();
 
-  cout << "finished output.\n";
-  outfile.close();
+    // sort tuples lexicographically, i.e., by time
+    cout << "finished raw req sequence.\n";
+    reqseq.sort();
+    cout << "finished sorting req.\n";
 
-  return 0;
+    ofstream outfile;
+    outfile.open(outputfile);
+    outfile<<fixed<<setprecision(0); // turn of scientific notation
+
+    // output request sequence
+    list_iterator_t rit;
+    for (rit=reqseq.begin(); rit != reqseq.end(); ++rit) {
+        outfile << 1000*get<0>(*rit) << " " << get<1>(*rit) << " " << size[get<1>(*rit)] << "\n"; 
+    }
+
+    cout << "finished output.\n";
+    outfile.close();
+
+    return 0;
 }
