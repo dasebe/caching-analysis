9c288da58d208fd351baaa70d2b4cf975a951d31
Daniel S Berger <dsberger@cmu.edu>
2019-08-01 18:34:50 -0400

    trace statistics over time script

--- analysis/basestats/stats.cpp
+++ analysis/basestats/stats.cpp
@@ -33,18 +33,19 @@ int main (int argc, char* argv[])
     unordered_map<string, uint64_t > totalobjs;
 
     std::string id;
-    int64_t t, size, tmp;
+    int64_t t, tmp;
+    uint64_t size;
     uint64_t totalReqs = 0;
     int64_t lastPrint = 0;
 
     while (infile >> t >> id >> size)
     {
+        // parsing
         for(uint64_t i=0; i<extraFields; i++) {
             // read other fields
             infile >> tmp;
         }
-        totalReqs++;
-        intstats["RequestCount"]++;
+        // check and print
         if(t - lastPrint > interval) { // in seconds
             for(auto & it: intstats) {
                 outfile << inputFile << " " << t << " " << it.first << " " << it.second << "\n";
@@ -53,11 +54,27 @@ int main (int argc, char* argv[])
             intobjs.clear();
             lastPrint = t;
         }
+        // stats
+        totalReqs++;
+        intstats["RequestCount"]++;
+        intstats["RequestedBytes"]+=size;
+        // unique objs
         if(intobjs.count(id)==0) {
             intobjs[id] = 1;
             intstats["UniqueObjects"]++;
+            intstats["UniqueBytes"]+=size;
         }
         totalobjs[id]++;
+        // osizes
+        if(intstats["ObjectSize_Min"]==0) {
+            intstats["ObjectSize_Min"] = size;
+        }
+        if(size<intstats["ObjectSize_Min"]) {
+            intstats["ObjectSize_Min"] = size;
+        }
+        if(size>intstats["ObjectSize_Max"]) {
+            intstats["ObjectSize_Max"] = size;
+        }
     }
 
     infile.close();
