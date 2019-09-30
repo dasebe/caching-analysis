b3f897a5ccde80325704b72b34f673a3edb08b42
Daniel S. Berger <dsberger@cmu.edu>
2019-09-30 22:07:31 +0000

    refactored: parser factory

---
+++ analysis/basestats/parse.h
@@ -0,0 +1,72 @@
+#pragma once
+
+#include <iostream>
+#include <memory>
+#include <fstream>
+#include <map>
+
+class Parser;
+
+class ParserFactory {
+public:
+    ParserFactory() {}
+    virtual std::unique_ptr<Parser> create_unique() = 0;
+};
+
+class Parser {
+protected:
+    const std::string traceName;
+    std::ifstream infile;
+    int extraFields;
+    ReqBatch batch;
+
+    // helper function for factory pattern
+    static std::map<std::string, ParserFactory *> &get_factory_instance() {
+        static std::map<std::string, ParserFactory *> map_instance;
+        return map_instance;
+    }
+
+public:
+    Parser()
+    {}
+    ~Parser() {
+        infile.close();
+    }
+    void setFile(std::string fname) {
+        infile.open(fname);
+    }
+    void setExtraFields(int nextraFields) {
+        extraFields = nextraFields;
+    }
+
+    virtual bool parseBatch(size_t parseReqs) = 0;
+    ReqBatch & getBatch() {
+        return batch;
+    }
+
+    // factory pattern
+    static void registerType(std::string name, ParserFactory *factory) {
+        get_factory_instance()[name] = factory;
+    }
+    static std::unique_ptr<Parser> create_unique(std::string name) {
+        std::unique_ptr<Parser> Parser_instance;
+        if(get_factory_instance().count(name) != 1) {
+            std::cerr << "unkown cacheType" << std::endl;
+            return nullptr;
+        }
+        Parser_instance = move(get_factory_instance()[name]->create_unique());
+        return Parser_instance;
+    }
+
+};
+            
+
+template<class T>
+class Factory : public ParserFactory {
+public:
+    Factory(std::string name) { Parser::registerType(name, this); }
+    std::unique_ptr<Parser> create_unique() {
+        std::unique_ptr<Parser> newT(new T);
+        return newT;
+    }
+};
