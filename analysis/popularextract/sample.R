5ba5bab910566727dac5fd2d627cf0764a7323e4
Daniel S. Berger <berger@cs.uni-kl.de>
2019-10-15 09:59:11 -0400

    old plotting script changes

--- analysis/popularextract/sample.R
+++ analysis/popularextract/sample.R
@@ -10,3 +10,23 @@ dt.sample <- dt[V3 %in% ids.sample]
 
 ggplot(dt.sample,aes(V2,V1,color=factor(V3)))+
     geom_point()
+
+
+# sampled
+
+ppath <- "sampled"
+files <- dir(ppath)
+
+for(i in files) {
+    print(i)
+    fn <- paste("sampled/",i,sep="")
+    dt <- data.table(read.table(fn))
+fnplot <- paste("sampleplots/",i,"_plots.pdf",sep="")
+pdf(fnplot,7,5)
+p <- ggplot(dt,aes(V2,V1))+
+    geom_point()+
+    scale_x_continuous("Time [unix ts]")+
+    scale_y_continuous("Server",breaks=0:9,labels=0:9)
+    print(p)
+    dev.off()
+}
