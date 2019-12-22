08803ae0ef0d62f283afbe129697fcb17ab6b312
Daniel S Berger <dsberger@cmu.edu>
2019-12-21 21:48:41 -0500

    final wikipedia 2019 trace parser

--- parsing/wikiraw/2019trace/rewrite_trace_wiki.cpp
+++ parsing/wikiraw/2019trace/rewrite_trace_wiki.cpp
@@ -11,75 +11,110 @@ int main (int argc, char* argv[])
 {
 
   // parameters
-  if(argc != 3) {
-    cerr << "usage:\n" << argv[0] << " infile outfile\n";
+    if(argc < 3) {
+    cerr << "usage:\n" << argv[0] << " outfile upload? infile1 infile2 ..\n";
     return 1;
   }
-
-  const char* inputFile = argv[1];
-  const char* outputMem = argv[2];
-
   cerr << "starting ..." << endl;
 
-  ifstream infile(inputFile);
+  const char* outputMem = argv[1];
+  // upload flag means there'll be an additional column to parse
+  bool upload = false;
+  string uploadpar(argv[2]);
+  if(uploadpar.compare("upload") == 0 || uploadpar.compare("1") == 0 || uploadpar.compare("yes") == 0) {
+      upload = true;
+      cerr << "upload trace\n";
+  }
   ofstream outfile(outputMem);
-  int64_t size, simpleId=0;
-  string strid, timestamp;
-  unordered_map<string,uint64_t> dSimpleId;
 
+  int64_t size, simpleId=0;
+  string strid, timestamp, strtype;
+  unordered_map<string,uint64_t> dSimpleId;
+  unordered_map<string,size_t> typeId;
   const char row_delim = '\n';
   const char field_delim = '\t';
   string row;
 
-  getline(infile, row, row_delim); // headers
+  uint64_t requestCount = 0;
 
-  for (; getline(infile, row, row_delim); ) {
-    // parse row
-    istringstream ss(row);
-    string field;
+  for(int i = 3; i<argc; i++) {
+      const char* inputFile = argv[i];
+      cerr << "parsing trace " << i << " file " << inputFile << "\n";
+      ifstream infile(inputFile);
 
-    // get timestamp
-    getline(ss, field, field_delim);
-    if(field.empty()) {
-      cerr << "empty timestamp " << row << endl;
-      continue;
-    }
-    timestamp = field;
-    field.clear();
+      // ignore first line of headers
+      getline(infile, row, row_delim);
 
-    // get ID
-    getline(ss, field, field_delim);
-    if(field.empty()) {
-      cerr << "empty id " << row << endl;
-      continue;
-    }
-    strid = field;
-    field.clear();
+      for (; getline(infile, row, row_delim); ) {
+          // parse row
+          istringstream ss(row);
+          string field;
 
-    // get size
-    field.clear();
-    getline(ss, field, field_delim);
-    if(field.empty()) {
-      cerr << "empty size " << row << endl;
-      continue;
-    }
-    stringstream fieldstream2( field );
-    fieldstream2 >> size;
+          // get timestamp
+          getline(ss, field, field_delim);
+          if(field.empty()) {
+              cerr << "empty timestamp " << row << endl;
+              continue;
+          }
+          timestamp = field;
+          field.clear();
 
-    // new id= origin_id + object_size + url_parameters
-    if(dSimpleId.count(strid)==0)
-      dSimpleId[strid]=simpleId++;
+          // get ID
+          getline(ss, field, field_delim);
+          if(field.empty()) {
+              cerr << "empty id " << row << endl;
+              continue;
+          }
+          strid = field;
+          field.clear();
 
-    // skip incomplete objects
-    if (size <= 1)
-      continue;
+          if(upload) {
+              // get type
+              getline(ss, field, field_delim);
+              if(field.empty()) {
+                  cerr << "empty type (id=" << strid << ")\n" << row << endl;
+                  continue;
+              }
+              strtype = field;
+              if(typeId.count(strtype)==0) {
+                  // new type
+                  const size_t idx = typeId.size();
+                  typeId[strtype] = idx;
+                  cerr << timestamp << " type " << strtype << " : " << typeId[strtype] << "\n";
+              }
+              field.clear();
+          }
 
-    outfile << timestamp << " " << dSimpleId[strid] << " " << size << "\n";
+          // get size
+          getline(ss, field, field_delim);
+          if(field.empty()) {
+              cerr << "empty size " << row << endl;
+              continue;
+          }
+          stringstream fieldstream2( field );
+          fieldstream2 >> size;
+          field.clear();
+
+          // ignore last field
+          getline(ss, field, field_delim);
+
+          // new id= origin_id + object_size + url_parameters
+          if(dSimpleId.count(strid)==0)
+              dSimpleId[strid]=simpleId++;
+
+          // skip incomplete objects
+          if (size <= 1)
+              continue;
+
+          requestCount++;
+          outfile << timestamp << " " << dSimpleId[strid] << " " << size << " " << typeId[strtype] << "\n";
+      }
+      infile.close();
+      cerr << "done with " << i << " file " << inputFile << " timestamp " << timestamp << " requests " << requestCount << " objects " << dSimpleId.size() << "\n";
   }
-  infile.close();
   outfile.close();
 
   cerr << "done" << endl;
-
+  
   return 0;
 }
